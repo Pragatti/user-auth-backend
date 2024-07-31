c0942be4fe1990e06ccc06ee5a2a991e3d4adbbf
Pragatti <pragatti4667@gmail.com>
2024-08-01 00:19:03 +0530

    redux toolkit setup

--- client/login-flow/src/App.jsx
+++ client/login-flow/src/App.jsx
@@ -1,13 +1,25 @@
-
+import React,{useState,useEffect} from "react"
+import { useDispatch} from 'react-redux'
+import './App.css'
+import { login } from "./store/authSlice"
 
 function App() {
- 
+ const[loading,setLoading] = useState(false)
+ const dispatch = useDispatch()
 
-  return (
-    <>
-    hello
-    </>
-  )
+ useEffect(()=>{ 
+  
+    dispatch(login("pragatti"))
+  
+ 
+  
+ },[])
+
+  return  !loading?<div className="min-h-screen flex">  hello
+
+  </div>:null;
+  
+  
 }
 
 export default App
